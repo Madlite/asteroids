36f74c606ba3a99f8738f022f6332c3f2e5da220
Madlite <mistermadlite@gmail.com>
2025-05-03 12:39:07 +0200

    inital display window

---
+++ main.py
@@ -0,0 +1,20 @@
+import pygame
+from constants import *
+
+
+def main():
+    print("Starting Asteroids!")
+    screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
+
+    while True:
+        screen.fill("black")
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                return
+
+
+        pygame.display.flip()
+
+if __name__ == "__main__":
+    main()
+
